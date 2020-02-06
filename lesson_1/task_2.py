8f4181e66fa770131ad9be98d974a876f15edd52
artem.stolbtsov <artem>
2020-02-06 18:24:05 +0200

    Task 2.2

--- lesson_1/task_2.py
+++ lesson_1/task_2.py
@@ -16,10 +16,31 @@ def compare_keys_2_1():
 def compare_keys_2_2():
     a = {'a': 1, 'b': 4, 't': 67}
     b = {'c': 4, 'e': 1, 'a': 4, 't': 7, 'y': 11}
-    join_keys = []
+    res = {}
+    keys_a = []
     keys_b = []
+    res = []
+    for i in a:
+        keys_a.append(i)
+    for i in b:
+        keys_b.append(i)
+
+    for i in keys_b:
+        if i in keys_a:
+            continue
+        elif i not in keys_a:
+            res.extend(i)
+            continue
+    print("Task 2.2: ", res)
+
+    # for i in a.keys():
+    #     if i in b.keys():
+    #         continue
+    #     if i not in a.keys():
+    #         res.popitem(i)
+    #         continue
+    # print(res)
 
 
-
-compare_keys_2_1()
+#compare_keys_2_1()
 compare_keys_2_2()
